312fa5540c6c1c20c4ec391bce53beb4c6d40ed7
himawanTIF <himawan1700018032@webmail.uad.ac.id>
2020-05-04 23:39:35 +0700

    buat index

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Selamat Datang di SIMPENKEMAS</title>
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="jumbotron-md text-center bg-success">
+            <h3>Selamat Datang di SIMPENKEMAS</h3>
+            <h4>Sistem Penilaian Keaktifan Mahasiswa</h4>
+            <h6>Program Studi Ilmu Keperawatan</h6>
+            <h6>Universitas Muhammadiyah Yogyakarta</h6>
+        </div>
+        <a href="dashboard.php">Dashboard</a>
+    </div>
+</body>
+
+</html>
