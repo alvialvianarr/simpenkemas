d7398fefc052dc26a10f84a2d6f2d7739622e5da
himawanTIF <himawan1700018032@webmail.uad.ac.id>
2020-05-04 17:02:53 +0700

    Update folder

--- dashboard.php
+++ dashboard.php
@@ -1,3 +1,13 @@
 <html>
 
+<head>
+    <title>Selamat Datang di SIPKEMAS</title>
+</head>
+
+<body>
+    <h1>Selamat Datang di SIPKEMAS</h1>
+    <h4>Sistem Penilaian Keaktifan Mahasiswa Program Studi Ilmu Keperawatan</h4>
+    <h5>Universitas Muhammadiyah Yogyakarta</h5>
+</body>
+
 </html>
