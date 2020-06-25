74c722c17250105f30e1bd6b346350f588cb57c9
himawanTIF <40656518+himawanTIF@users.noreply.github.com>
2020-06-25 15:27:48 +0700

    Update 25/06/20

---
+++ navbar.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="#">
+    <title></title>
+</head>
+<header>
+    <!-- Navbar -->
+    <nav class="navbar navbar-dark fixed-top bg-success flex-md-nowrap p-0 shadow">
+        <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="dashboard.php"
+            style="font-size: 1rem;line-height: 31px;letter-spacing: 0.039em;font-weight: 400;">Sistem Penilaian
+            Online</a>
+        <div class="container-fluid flex-auto">
+            <a href="https://www.umy.ac.id" class="navbar-brand col-lg-12 mr-0"
+                style="font-size: 0.9rem;line-height: 0px;letter-spacing: -0.003em;font-weight: 400;font-style: normal;">
+                <img src="assets/img/logo_umy.png" alt="logo" style="width: 32px">
+                Universitas Muhammadiyah Yogyakarta</a>
+        </div>
+    </nav>
+    <!-- End Navbar -->
+</header>
+</html>
